b50a22be914481605026eb9c30da13f0bda06dbd
Joris Kuipers <joriskuipers@gmail.com>
2011-12-14 16:41:53 +0100

    Removed needless root context, simplified Java config

--- src/main/java/org/resthub/todo/TodoInitializer.java
+++ src/main/java/org/resthub/todo/TodoInitializer.java
@@ -3,7 +3,6 @@ package org.resthub.todo;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletRegistration;
 import org.springframework.web.WebApplicationInitializer;
-import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
@@ -11,13 +10,6 @@ public class TodoInitializer implements WebApplicationInitializer {
 
     @Override
     public void onStartup(ServletContext servletContext) {
-
-        // Create the 'root' Spring application context
-        AnnotationConfigWebApplicationContext rootContext = new AnnotationConfigWebApplicationContext();
-        rootContext.register(WebConfig.class);
-
-        // Manage the lifecycle of the root application context
-        servletContext.addListener(new ContextLoaderListener(rootContext));
 
         // Create the dispatcher servlet's Spring application context
         AnnotationConfigWebApplicationContext dispatcherContext = new AnnotationConfigWebApplicationContext();
