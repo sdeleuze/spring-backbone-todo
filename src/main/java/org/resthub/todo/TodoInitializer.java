9e4d65cde3555dc0ee80eb8744b166c0db0c5277
Sébastien Deleuze <seb@deleuze.fr>
2012-04-18 17:11:08 +0200

    Support Jetty Maven plugin

--- src/main/java/org/resthub/todo/TodoInitializer.java
+++ src/main/java/org/resthub/todo/TodoInitializer.java
@@ -1,24 +1,24 @@
 package org.resthub.todo;
 
 import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
 import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
 public class TodoInitializer implements WebApplicationInitializer {
-
+   
     @Override
-    public void onStartup(ServletContext servletContext) {
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        AnnotationConfigWebApplicationContext rootContext = new AnnotationConfigWebApplicationContext();
+        rootContext.register(WebConfig.class);
 
-        // Create the dispatcher servlet's Spring application context
-        AnnotationConfigWebApplicationContext dispatcherContext = new AnnotationConfigWebApplicationContext();
-        dispatcherContext.register(WebConfig.class);
-
-        // Register and map the dispatcher servlet
-        ServletRegistration.Dynamic dispatcher = servletContext.addServlet("dispatcher", new DispatcherServlet(dispatcherContext));
+        ServletRegistration.Dynamic dispatcher = servletContext.addServlet("dispatcher", new DispatcherServlet(rootContext));
         dispatcher.setLoadOnStartup(1);
-        dispatcher.addMapping("/");
-    }   
+        dispatcher.addMapping("/*");
+        servletContext.addListener(new ContextLoaderListener(rootContext));
+    }  
    
 }
